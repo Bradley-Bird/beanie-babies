b9093e3b5c0af96e6fc2a60ed7075639780a1501
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-25 14:15:48 -0700

    added logic for displaying prev page only when greater than one

--- src/BeaniesPage.js
+++ src/BeaniesPage.js
@@ -28,7 +28,7 @@ function App() {
       <div className="buttons">
         {/* on click, this button should decrement the page in state  */}
         {/* also, disable this button when you are on the first page */}
-        <button onClick={() => setPage((prev) => prev - 1)}>Previous Page</button>
+        {page > 1 && <button onClick={() => setPage((prev) => prev - 1)}>Previous Page</button>}
         {/* on click, this button should increment the page in state  */}
         <button onClick={() => setPage((prev) => prev + 1)}>Next Page</button>
       </div>
